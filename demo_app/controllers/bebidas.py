d04dbc21c28c6631a0d20c5eb80b665dfe09b65e
CrisErazo2001 <acerazobaroja@gmail.com>
2024-09-05 20:45:33 -0500

    bebida2

--- demo_app/controllers/bebidas.py
+++ demo_app/controllers/bebidas.py
@@ -57,21 +57,22 @@ def get_all_bebidas():
     
     return jsonify(bebidas)
 
-@app.route('/bebida/get/id',methods=['POST'])
+@app.route('/bebida/get/id')
 def get_bebida_by_id():
-       
+    
+    print(request.args)
     data = {
-        'id_bebidas': request.form["id_bebidas"]
+        'id_bebidas': request.args["id_bebidas"]
     }
     result = bebida.get_by_id(data)
     result = result.asdict()
     return jsonify(result)
 
-@app.route('/bebida/get/name',methods=['POST'])
+@app.route('/bebida/get/name')
 def get_bebida_by_name():
        
     data = {
-        'nombre': request.form["nombre"]
+        'nombre': request.args["nombre"]
     }
     result = bebida.get_by_name(data)
     result = result.asdict()
@@ -96,3 +97,79 @@ def delete_bebida_by_name():
     result = bebida.delete_by_name(data)
     print(result)
     return redirect('/bebida/delete')
+
+@app.route('/bebida/modify/id',methods=['POST'])
+def update_bebida_by_id():
+       
+    data = {
+        'id_bebidas': request.form['id_bebidas'],
+        'Pos_1': request.form["Pos_1"],
+        'Pos_2': request.form["Pos_2"],
+        'Pos_3': request.form['Pos_3'],
+        'Pos_4': request.form['Pos_4'],
+        'Pos_5': request.form['Pos_5'],
+        'Pos_6': request.form['Pos_6'],
+        'Pos_7': request.form['Pos_7'],
+        'Pos_8': request.form['Pos_8'],
+        'Pos_9': request.form['Pos_9'],
+        'Pos_10': request.form['Pos_10'],
+        'Pos_11': request.form['Pos_11'],
+        'Pos_12': request.form['Pos_12'],
+        'Pos_13': request.form['Pos_13'],
+        'Pos_14': request.form['Pos_14'],
+        'Pos_15': request.form['Pos_15'],
+        'Pos_16': request.form['Pos_16'],
+        'Pos_17': request.form['Pos_17'],
+        'Pos_18': request.form['Pos_18'],
+        'Pos_19': request.form['Pos_19'],
+        'Pos_20': request.form['Pos_20'],
+        'Pos_21': request.form['Pos_21'],
+        'Pos_22': request.form['Pos_22'],
+        'Pos_23': request.form['Pos_23'],
+        'Pos_24': request.form['Pos_24'],
+        'Pos_25': request.form['Pos_25'],
+        'Pos_26': request.form['Pos_26'],
+        'Pos_27': request.form['Pos_27'],
+        'nombre': request.form['nombre']
+    }
+    result = bebida.update_by_id(data)
+    print(result)
+    return redirect('/bebida')
+
+@app.route('/bebida/delete/name',methods=['POST'])
+def update_bebida_by_name():
+       
+    data = {
+        
+        'Pos_1': request.form["Pos_1"],
+        'Pos_2': request.form["Pos_2"],
+        'Pos_3': request.form['Pos_3'],
+        'Pos_4': request.form['Pos_4'],
+        'Pos_5': request.form['Pos_5'],
+        'Pos_6': request.form['Pos_6'],
+        'Pos_7': request.form['Pos_7'],
+        'Pos_8': request.form['Pos_8'],
+        'Pos_9': request.form['Pos_9'],
+        'Pos_10': request.form['Pos_10'],
+        'Pos_11': request.form['Pos_11'],
+        'Pos_12': request.form['Pos_12'],
+        'Pos_13': request.form['Pos_13'],
+        'Pos_14': request.form['Pos_14'],
+        'Pos_15': request.form['Pos_15'],
+        'Pos_16': request.form['Pos_16'],
+        'Pos_17': request.form['Pos_17'],
+        'Pos_18': request.form['Pos_18'],
+        'Pos_19': request.form['Pos_19'],
+        'Pos_20': request.form['Pos_20'],
+        'Pos_21': request.form['Pos_21'],
+        'Pos_22': request.form['Pos_22'],
+        'Pos_23': request.form['Pos_23'],
+        'Pos_24': request.form['Pos_24'],
+        'Pos_25': request.form['Pos_25'],
+        'Pos_26': request.form['Pos_26'],
+        'Pos_27': request.form['Pos_27'],
+        'nombre': request.form['nombre']
+    }
+    result = bebida.update_by_name(data)
+    print(result)
+    return redirect('/bebida')
--- demo_app/controllers/bebidas_ui.py
+++ demo_app/controllers/bebidas_ui.py
@@ -4,6 +4,7 @@ from demo_app import app
 from demo_app.models.bebida import bebida
 from flask_bcrypt import Bcrypt
 import datetime
+import requests
 bcrypt = Bcrypt(app)
 app.secret_key = 'keep it secret, keep it safe'
 
@@ -21,3 +22,26 @@ def getbebidas():
 def deletebebidas():
     
         return render_template("bebida_delete.html")
+
+@app.route('/bebida/modify/<id>/<name>')
+def updatebebidas(id, name):
+        id = int(id)
+        print(id)
+        print(name)
+        dataId = {
+                'id_bebidas': id
+        }
+        dataName = {
+                'nombre': name
+        }
+        url1 = 'http://127.0.0.1:5000/bebida/get/id'
+        url2 = 'http://127.0.0.1:5000/bebida/get/name'
+        r1 = requests.get(url1, params= dataId)
+        r2 = requests.get(url2, params= dataName)
+        result1 = r1.json()
+        result2 = r2.json()
+        print(result1)
+        print(result2)
+        return render_template("bebida_modify.html", id = id, name = name
+                              ,id_bebidas=result1['id_bebidas'],nombre = result1['nombre'] , Pos_1 = result1['Pos_1'] ,Pos_2 =   result1['Pos_2'] ,Pos_3 = result1['Pos_3'] ,Pos_4 = result1['Pos_4'] ,Pos_5 = result1['Pos_5'] ,Pos_6 = result1['Pos_6'] ,Pos_7 = result1['Pos_7'] ,Pos_8 = result1['Pos_8'] ,Pos_9 = result1['Pos_9'] ,Pos_10 = result1['Pos_10'] ,Pos_11 = result1['Pos_11'] ,Pos_12 = result1['Pos_12'] ,Pos_13 = result1['Pos_13'] ,Pos_14 = result1['Pos_14'] ,Pos_15 = result1['Pos_15'] ,Pos_16 = result1['Pos_16'] ,Pos_17 = result1['Pos_17'] ,Pos_18 = result1['Pos_18'] ,Pos_19 = result1['Pos_19'] ,Pos_20 = result1['Pos_20'] ,Pos_21 = result1['Pos_21'] ,Pos_22 = result1['Pos_22'] ,Pos_23 = result1['Pos_23'] ,Pos_24 = result1['Pos_24'] ,Pos_25 = result1['Pos_25'] ,Pos_26 = result1['Pos_26'] ,Pos_27 = result1['Pos_27'], 
+                               nombre_n = result1['nombre'] , Pos_1_n = result2['Pos_1'] ,Pos_2_n =   result2['Pos_2'] ,Pos_3_n = result2['Pos_3'] ,Pos_4_n = result2['Pos_4'] ,Pos_5_n = result2['Pos_5'] ,Pos_6_n = result2['Pos_6'] ,Pos_7_n = result2['Pos_7'] ,Pos_8_n = result2['Pos_8'] ,Pos_9_n = result2['Pos_9'] ,Pos_10_n = result2['Pos_10'] ,Pos_11_n = result2['Pos_11'] ,Pos_12_n = result2['Pos_12'] ,Pos_13_n = result2['Pos_13'] ,Pos_14_n = result2['Pos_14'] ,Pos_15_n = result2['Pos_15'] ,Pos_16_n = result2['Pos_16'] ,Pos_17_n = result2['Pos_17'] ,Pos_18_n = result2['Pos_18'] ,Pos_19_n = result2['Pos_19'] ,Pos_20_n = result2['Pos_20'] ,Pos_21_n = result2['Pos_21'] ,Pos_22_n = result2['Pos_22'] ,Pos_23_n = result2['Pos_23'] ,Pos_24_n = result2['Pos_24'] ,Pos_25_n = result2['Pos_25'] ,Pos_26_n = result2['Pos_26'] ,Pos_27_n = result2['Pos_27'])
