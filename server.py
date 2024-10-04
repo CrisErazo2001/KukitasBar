60ca948cc3be5c6d883ef20ea55383370547ed32
DoreanJ <dojoacma@gmail.com>
2024-10-04 12:36:47 -0500

    Mio

--- server.py
+++ server.py
@@ -12,19 +12,16 @@ donde {venv} es el nombre de su entorno virtual
 
 
 from demo_app import app
-#from flask_cors import CORS
+# from flask_cors import CORS
 import requests
 
-from demo_app.controllers import bebidas_ui, dashboard, posicion_bebidas,lista_bebidas,recetas,pedidos,users
-
-
+from demo_app.controllers import bebidas_ui, dashboard, posicion_bebidas, lista_bebidas, recetas, pedidos, users
 
 
 APP = app
-#CORS(APP)
+# CORS(APP)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     APP.run(debug=True)
     # APP.run(debug=True,host='000.000.000.000') descomentar esta linea para arrancar el sistema desde la red y no solo en local asi se puede acceder desde cualquier punto de la red, cambiar host con la ip del servidor
-    
